942b3e834aeb5ce873a0d2b577a964e91b3a722e
Chris <44344493+ChrisDaDerp@users.noreply.github.com>
2023-06-11 22:14:32 -0400

    added common names for pokemon

--- pokemon_stats.py
+++ pokemon_stats.py
@@ -3,6 +3,7 @@ This script will gather all the stats of Pokémon you supply it with and print o
 in a table (ordered by any stat you want; default is speed) with all stats of all the Pokémon.
 This will be very useful for draft leagues so you don't have to calculate the
 speed of every single Pokémon you will be facing individually.
+This script is up to date for up to Gen IX, as of 2023-06-11.
 
 @Author ChrisDaDerp
 '''
@@ -32,6 +33,180 @@ BLUE = "\u001b[34m"
 BOLD = "\033[1m"
 END = "\033[0m"
 
+# Common names that may probably be typo'd because of how PokeAPI stores them.
+# Also some common shorthands for Pokémon that may be entered for convenience.
+# These will be assumed names and may not be what are desired. Writing the exact name will bypass the assumption.
+COMMON_NAMES = {}
+COMMON_NAMES["nidoran"] = "nidoran-m"
+COMMON_NAMES["farfetch'd"] = "farfetchd"
+COMMON_NAMES["mrmime"] = "mr-mime"
+COMMON_NAMES["mr.mime"] = "mr-mime"
+COMMON_NAMES["deoxys"] = "deoxys-normal"
+COMMON_NAMES["deoxys-n"] = "deoxys-normal"
+COMMON_NAMES["deoxys-a"] = "deoxys-attack"
+COMMON_NAMES["deoxys-d"] = "deoxys-defense"
+COMMON_NAMES["deoxys-s"] = "deoxys-speed"
+COMMON_NAMES["wormadam"] = "wormadam-plant"
+COMMON_NAMES["mimejr"] = "mime-jr"
+COMMON_NAMES["mime.jr"] = "mime-jr"
+COMMON_NAMES["giratina"] = "giratina-origin"
+COMMON_NAMES["giratina-a"] = "giratina-altered"
+COMMON_NAMES["giratina-o"] = "giratina-origin"
+COMMON_NAMES["shaymin"] = "shaymin-sky"
+COMMON_NAMES["shaymin-l"] = "shaymin-land"
+COMMON_NAMES["shaymin-s"] = "shaymin-land"
+COMMON_NAMES["basculin"] = "basculin-red-striped"
+COMMON_NAMES["darmanitan"] = "darmanitan-standard"
+COMMON_NAMES["darm"] = "darmanitan-standard"
+COMMON_NAMES["darmanitan-s"] = "darmanitan-standard"
+COMMON_NAMES["darm-s"] = "darmanitan-standard"
+COMMON_NAMES["darmanitan-z"] = "darmanitan-zen"
+COMMON_NAMES["darm-z"] = "darmanitan-zen"
+COMMON_NAMES["tornadus"] = "tornadus-therian"
+COMMON_NAMES["tornadus-t"] = "tornadus-therian"
+COMMON_NAMES["tornadus-i"] = "tornadus-incarnate"
+COMMON_NAMES["thundurus"] = "thundurus-incarnate"
+COMMON_NAMES["thundurus-i"] = "thundurus-incarnate"
+COMMON_NAMES["thundurus-t"] = "thundurus-therian"
+COMMON_NAMES["landorus"] = "landorus-therian"
+COMMON_NAMES["landorus-t"] = "landorus-therian"
+COMMON_NAMES["lando-t"] = "landorus-therian"
+COMMON_NAMES["landorus-i"] = "landorus-incarnate"
+COMMON_NAMES["keldeo"] = "keldeo-ordinary"
+COMMON_NAMES["meloetta"] = "meloetta-aria"
+COMMON_NAMES["meloetta-a"] = "meloetta-aria"
+COMMON_NAMES["meloetta-p"] = "meloetta-pirouette"
+COMMON_NAMES["meowstic"] = "meowstic-male"
+COMMON_NAMES["pumpkaboo"] = "pumpkaboo-base"
+COMMON_NAMES["gourgeist"] = "gourgeist-base"
+COMMON_NAMES["zygarde"] = "zygarde-base"
+COMMON_NAMES["zygarde-b"] = "zygarde-base"
+COMMON_NAMES["zygarde-c"] = "zygarde-complete"
+COMMON_NAMES["zygarde-10"] = "zygarde-10"
+COMMON_NAMES["oricorio"] = "oricorio-baile"
+COMMON_NAMES["oricorio-b"] = "oricorio-baile"
+COMMON_NAMES["oricorio-pp"] = "oricorio-pom-pom"
+COMMON_NAMES["oricorio-pa"] = "oricorio-pau"
+COMMON_NAMES["oricorio-s"] = "oricorio-sensu"
+COMMON_NAMES["lycanroc"] = "lycanroc-dusk"
+COMMON_NAMES["lycanroc-d"] = "lycanroc-dusk"
+COMMON_NAMES["lycanroc-md"] = "lycanroc-midday"
+COMMON_NAMES["lycanroc-mn"] = "lycanroc-midnight"
+COMMON_NAMES["wishiwashi"] = "wishiwashi-school"
+COMMON_NAMES["type:null"] = "type-null"
+COMMON_NAMES["type_null"] = "type-null"
+COMMON_NAMES["typenull"] = "type-null"
+COMMON_NAMES["minior"] = "minior-core"
+COMMON_NAMES["minior-c"] = "minior-core"
+COMMON_NAMES["minior-m"] = "minior-meteor"
+COMMON_NAMES["mimikyu"] = "mimikyu-disguised"
+COMMON_NAMES["necrozma-dusk"] = "necrozma-dusk-mane"
+COMMON_NAMES["necrozma-dawn"] = "necrozma-dawn-wings"
+COMMON_NAMES["rattata-a"] = "rattata-alola"
+COMMON_NAMES["raticate-a"] = "raticate-alola"
+COMMON_NAMES["raichu-a"] = "raichu-alola"
+COMMON_NAMES["sandshrew-a"] = "sandshrew-alola"
+COMMON_NAMES["sandslash-a"] = "sandslash-alola"
+COMMON_NAMES["vulpix-a"] = "vulpix-alola"
+COMMON_NAMES["nintales-a"] = "ninetales-alola"
+COMMON_NAMES["diglett-a"] = "diglett-alola"
+COMMON_NAMES["dugtrio-a"] = "dugtrio-alola"
+COMMON_NAMES["meowth-a"] = "meowth-alola"
+COMMON_NAMES["persian-a"] = "persian-alola"
+COMMON_NAMES["geodude-a"] = "geodude-alola"
+COMMON_NAMES["graveler-a"] = "graveler-alola"
+COMMON_NAMES["golem-a"] = "golem-alola"
+COMMON_NAMES["grimer-a"] = "grimer-alola"
+COMMON_NAMES["muk-a"] = "muk-alola"
+COMMON_NAMES["exeggutor-a"] = "exeggutor-alola"
+COMMON_NAMES["marowak-a"] = "marowak-alola"
+COMMON_NAMES["tapukoko"] = "tapu-koko"
+COMMON_NAMES["koko"] = "tapu-koko"
+COMMON_NAMES["tapulele"] = "tapu-lele"
+COMMON_NAMES["lele"] = "tapu-lele"
+COMMON_NAMES["tapubulu"] = "tapu-bulu"
+COMMON_NAMES["bulu"] = "tapu-bulu"
+COMMON_NAMES["tapufini"] = "tapu-fini"
+COMMON_NAMES["fini"] = "tapu-fini"
+COMMON_NAMES["toxtricity"] = "toxtricity-amped"
+COMMON_NAMES["zigzagoon-g"] = "zigzagoon-galar"
+COMMON_NAMES["linoone-g"] = "linoone-galar"
+COMMON_NAMES["meowth-g"] = "meowth-galar"
+COMMON_NAMES["ponyta-g"] = "ponyta-galar"
+COMMON_NAMES["rapidash-g"] = "rapidash-galar"
+COMMON_NAMES["slowpoke-g"] = "slowpoke-galar"
+COMMON_NAMES["slowbro-g"] = "slowbro-galar"
+COMMON_NAMES["slowking-g"] = "slowking-galar"
+COMMON_NAMES["corsola-g"] = "corsola-galar"
+COMMON_NAMES["farfetch'd-g"] = "farfetchd-galar"
+COMMON_NAMES["farfetchd-g"] = "farfetched-galar"
+COMMON_NAMES["sirfetch'd"] = "sirfetchd"
+COMMON_NAMES["weezing-g"] = "weezing-galar"
+COMMON_NAMES["mrmime-g"] = "mr-mime-galar"
+COMMON_NAMES["mr.mime-g"] = "mr-mime-galar"
+COMMON_NAMES["mrrime"] = "mr-rime"
+COMMON_NAMES["mr.rime"] = "mr-rime"
+COMMON_NAMES["darumaka-g"] = "darumaka-galar"
+COMMON_NAMES["darmanitan-g"] = "darmanitan-galar-standard"
+COMMON_NAMES["darm-g"] = "darmanitan-galar-standard"
+COMMON_NAMES["darmanitan-g-s"] = "darmanitan-galar-standard"
+COMMON_NAMES["darm-g-s"] = "darmanitan-galar-standard"
+COMMON_NAMES["darmanitan-g-z"] = "darmanitan-galar-zen"
+COMMON_NAMES["darm-g-z"] = "darmanitan-galar-zen"
+COMMON_NAMES["yamask-g"] = "yamask-galar"
+COMMON_NAMES["stunfisk-g"] = "stunfisk-galar"
+COMMON_NAMES["eiscue"] = "eiscue-ice"
+COMMON_NAMES["eiscue-n"] = "eiscue-noice"
+COMMON_NAMES["indeedee"] = "indeedee-male"
+COMMON_NAMES["indeedee-m"] = "indeedee-male"
+COMMON_NAMES["indeedee-f"] = "indeedee-female"
+COMMON_NAMES["morpeko"] = "morpeko-full-belly"
+COMMON_NAMES["morpeko-f"] = "morpeko-full-belly"
+COMMON_NAMES["morpeko-h"] = "morpeko-hangry"
+COMMON_NAMES["urshifu"] = "urshifu-single-strike"
+COMMON_NAMES["urshifu-s"] = "urshifu-single-strike"
+COMMON_NAMES["urshifu-ss"] = "urshifu-single-strike"
+COMMON_NAMES["urshifu-r"] = "urshifu-single-strike"
+COMMON_NAMES["urshifu-rs"] = "urshifu-single-strike"
+COMMON_NAMES["articuno-g"] = "articuno-galar"
+COMMON_NAMES["zapdos-g"] = "zapdos-galar"
+COMMON_NAMES["moltres-g"] = "moltres-galar"
+# I can't find how the Paldean Tauros forms are stored on PokeAPI so I am excluding them.
+COMMON_NAMES["wooper-p"] = "wooper-paldea"
+COMMON_NAMES["palafin-h"] = "palafin-hero"
+COMMON_NAMES["greattusk"] = "great-tusk"
+COMMON_NAMES["tusk"] = "great-tusk"
+COMMON_NAMES["screamtail"] = "scream-tail"
+COMMON_NAMES["brutebonnet"] = "brute-bonnet"
+COMMON_NAMES["fluttermane"] = "flutter-mane"
+COMMON_NAMES["mane"] = "flutter-mane"
+COMMON_NAMES["slitherwing"] = "slither-wing"
+COMMON_NAMES["sandyshocks"] = "sandy-shocks"
+COMMON_NAMES["irontreads"] = "iron-treads"
+COMMON_NAMES["treads"] = "iron-treads"
+COMMON_NAMES["ironbundle"] = "iron-bundle"
+COMMON_NAMES["bundle"] = "iron-bundle"
+COMMON_NAMES["ironhands"] = "iron-hands"
+COMMON_NAMES["hands"] = "iron-hands"
+COMMON_NAMES["ironjugulis"] = "iron-jugulis"
+COMMON_NAMES["jugulis"] = "iron-jugulis"
+COMMON_NAMES["ironmoth"] = "iron-moth"
+COMMON_NAMES["moth"] = "iron-moth"
+COMMON_NAMES["ironthorns"] = "iron-thorns"
+COMMON_NAMES["thorns"] = "iron-thorns"
+COMMON_NAMES["wochien"] = "wo-chien"
+COMMON_NAMES["chienpao"] = "chien-pao"
+COMMON_NAMES["tinglu"] = "ting-lu"
+COMMON_NAMES["chiyu"] = "chi-yu"
+COMMON_NAMES["roaringmoon"] = "roaring-moon"
+COMMON_NAMES["moon"] = "roaring-moon"
+COMMON_NAMES["ironvaliant"] = "iron-valiant"
+COMMON_NAMES["valiant"] = "iron-valiant"
+COMMON_NAMES["walkingwake"] = "walking-wake"
+COMMON_NAMES["wake"] = "walking-wake"
+COMMON_NAMES["ironleaves"] = "iron-leaves"
+COMMON_NAMES["leaves"] = "iron-leaves"
+
 
 class Pokemon:
     '''
@@ -42,6 +217,9 @@ class Pokemon:
     '''
     def __init__(self, name, is_my_team):
         self.__name = name.lower()
+        # Replaces name with assumed name to avoid errors from mistakes.
+        if name.lower() in COMMON_NAMES:
+            self.__name = COMMON_NAMES[name]
 
         pokemon = requests.get("https://pokeapi.co/api/v2/pokemon/" + self.__name.lower())
         # If the API returns data, the Pokemon object is made.
@@ -78,12 +256,12 @@ class Pokemon:
         else:
             self.__is_valid = False
             if self.__name == "":
-                print(RED + BOLD + "No Pokémon were entered. Continuing.")
+                print(RED + BOLD + "No Pokémon was entered. Continuing.")
             else:
                 link = 'https://pokeapi.co/'
                 print(RED + BOLD + "ERROR: '" + self.__name + "' was not found when consulting PokeAPI (" + 
                             link + END + RED + 
-                            "). Check for a typo, or if the Pokémon has alternate forms. (E.g. 'landorus' should either be 'landorus-incarnate' or 'landorus-therian'.)" + END)
+                            "). Check for a typo, or if the Pokémon has alternate forms." + END)
 
     def __parse_name(self, name):
         '''
@@ -112,10 +290,10 @@ class Pokemon:
         
         # All Pokémon with alternate forms.
         hyphenated_names = ["ho-oh", "porygon-z", "wo-chien", "chi-yu", "chien-pao", "ting-lu"]
-        alternate_forms = ["tauros", "castform", "kyogre", "groudon", "deoxys", "burmy", "wormadam", "cherrim", "rotom", "dialga", "palkia", "giratina",
-                           "shaymin", "basculin", "darmanitan", "tornadus", "thundurus", "landorus", "enamorus", "kyruem", "meloetta", "greninja", "aegislash",
-                           "pumpkaboo", "gourgeist", "zygarde", "hoopa", "lycanroc", "wishiwashi", "minior", "necrozma", "toxtricity", "eiscue", "zacian",
-                           "zamazenta", "eternatus", "urhsifu", "calyrex", "palafin", "gimmighoul"]
+        alternate_forms = ["nidoran", "tauros", "castform", "kyogre", "groudon", "deoxys", "burmy", "wormadam", "cherrim", "rotom", "dialga", "palkia", "giratina",
+                           "shaymin", "basculin", "darmanitan", "tornadus", "thundurus", "landorus", "enamorus", "kyruem", "keldeo", "meloetta", "greninja", "aegislash",
+                           "pumpkaboo", "gourgeist", "zygarde", "hoopa", "lycanroc", "jangmo-o", "hakamo-o", "kommo-o" "wishiwashi", "minior", "necrozma", "toxtricity",
+                           "eiscue", "zacian", "zamazenta", "eternatus", "urhsifu", "calyrex", "palafin", "gimmighoul"]
         region_names = ["mega", "alola", "galar", "hisui", "paldea"]
         
         # Names with multiple parts and/or without special characters.
@@ -245,6 +423,7 @@ def main():
     sorted_pokemon_list = compare_teams(my_team, opponent_team)
 
     print_pokemon_list(sorted_pokemon_list)
+    input("Press any key to exit...")
 
 
 if __name__ == "__main__":
